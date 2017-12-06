6a234ababccb02f4498e910d662a9b8c021a07f5
burkez123 <31449966+burkez123@users.noreply.github.com>
2017-12-06 10:41:41 -0800

    Add files via upload

---
+++ YourInitials.java
@@ -0,0 +1,14 @@
+
+public class YourInitials 
+{
+	public static void main (String[] args)
+		{
+			System.out.println( "ZZZZZZ   JJJJJ    BBBB" );
+			System.out.println( "    Z      J      B   B" );
+			System.out.println( "   Z       J      B   B" );
+			System.out.println( "  Z        J      BBBB" );
+			System.out.println( " Z       J J      B   B" );
+			System.out.println( "ZZZZZZ    JJ      B   B" );
+			System.out.println( "                  BBBB" );
+		}
+}
